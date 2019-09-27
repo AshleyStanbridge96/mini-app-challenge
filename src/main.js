55c66e17133fbd321dff2977e0e5f6b1f3bf8893
Ashley Stanbridge <ashley_stanbridge@yahoo.com>
2019-09-27 12:51:25 -0500

    Added logic for calculations

--- src/main.js
+++ src/main.js
@@ -1,37 +1,20 @@
 //Function layout
-const calcTotal = (billAmt, tipPer, numPeople) => {
-    //Divide by the number of people to find total amount for each person
-    const newTotal = (billAmt / numPeople);
-
-    //Find tip amount for person(s)
-    const tipAmt = newTotal * tipPer;
-
-    //Overall total with tip included
-    const overallTotal = newTotal + tipAmt;
-
-    return overallTotal;
+const calcMinString = (firstString, secondString, thirdString) => {
+    const minLength = Math.min(firstString.length, secondString.length, thirdString.length);
+    return minLength;
 };
 
 
 document.querySelector("#calcButton").addEventListener("click", () => {
-    //Declare variables here and convert strings to numbers or floats
-    //Replace ID with correct name
-    const bAmt = parseFloat(document.querySelector("#bill-amt").value);
-    const tAmt = parseFloat(document.querySelector("#tipPercent").value);
-    const pAmt = parseInt(document.querySelector("#numPeople").value);
+    //Converted strings to int
+    const firstString = parseInt(document.querySelector("#firstString").value);
+    const secondString = parseInt(document.querySelector("#secondString").value);
+    const thirdString = parseInt(document.querySelector("#thirdString").value);
 
     //Begin of calculations
-    //Declare variables to store output of calculations
-    const billTotal = Math.round(calcTotal(bAmt, tAmt, pAmt) * 100) / 100;
-    const tipTotal = Math.round(calcTip(bAmt, tAmt, pAmt) * 100) / 100;
-
-    //Creating variables to be sent to IDs below
-    const tip = `Tip Total: $ ${tipTotal}`;
-    const total = `Bill Total: $ ${billTotal}`
+    const minString = calcMinString(firstString, secondString, thirdString);
 
     //Send results back to IDs
-    document.querySelector("#finalTipAmt").innerHTML = tip;
-    document.querySelector("#finalTotalAmt").innerHTML = total;
-    document.querySelector("#result").innerHTML = total;
+    document.querySelector("#result").innerHTML = minString;
 
 });
